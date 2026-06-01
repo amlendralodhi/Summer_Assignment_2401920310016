3df37d4751603e027549df3514dc0c5bc10c8262
Amlendra Lodhi <amlendralodhi732@gmail.com>
2026-06-01 12:53:33 +0530

    Create Minimum_cost_of_buying_candies_with_discount.cpp

---
+++ week_1/Day_1/Minimum_cost_of_buying_candies_with_discount.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+
+        int sum = 0;
+        int count = 0;
+        sort(cost.begin(),cost.end());
+
+        for(int i = cost.size()-1;i >= 0;i--)
+        {
+            if(count == 2)
+            {
+                count = 0;
+                continue;
+                
+            }
+            else{
+                sum += cost[i];
+                count++;
+            }
+        }
+        return sum;
+        
+    }
+};
